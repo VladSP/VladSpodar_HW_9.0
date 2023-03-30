68b8ff80431a0ca82c5ef2adc8aa008402d7c691
VladSP <sp.vlad96@gmail.com>
2023-03-30 12:37:06 +0300

    Second cimmit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
-let arrA = [];
-let arrB = [];
-let length = prompt('Введіть довжину масиву: ');
-for (let i = 0; i < length; i++) {
-  arrA.push(Math.floor(Math.random() * 100)); 
-}
-for (let i = 0; i < arrA.length; i++) {
-  let isPrime = true;
-  for (let j = 2; j <= Math.sqrt(arrA[i]); j++) {
-    if (arrA[i] % j === 0) {
-      isPrime = false;
-      break;
+arrA = new Array(+prompt('Введіть довжину масиву: '));
+arrB = [];
+toLabel:
+for(i = 0; i < arrA.length; i++) {
+    arrA[i] = Math.floor(Math.random()*100);
+
+    for(j = 2; j < arrA[i]; j++) {
+        if (arrA[i] % j == 0) {
+            continue toLabel;
+        }
     }
-  }
-  if (isPrime) {
-    arrB.push(arrA[i]);
-  }
-}
-let min = arrB[0];
-let max = arrB[0];
-for (let i = 1; i < arrB.length; i++) {
-  if (arrB[i] < min) {
-    min = arrB[i];
-  }
-  if (arrB[i] > max) {
-    max = arrB[i];
-  }
-}
+    arrB[i] = arrA[i];
+};
+
 console.log(arrA);
+document.write(arrA + "<br>");
+arrB = arrB.filter(Boolean);
 console.log(arrB);
-console.log(`Мінімальне просте число: ${min}`);
-console.log(`Максимальне просте число: ${max}`);
-document.write(`${arrA.join(', ')}<br>`);
-document.write(`${arrB.join(', ')}<br>`);
-document.write(`Мінімальне просте число: ${min}<br>`);
-document.write(`Максимальне просте число: ${max}<br>`);
+document.write(arrB + "<br>");
+
+minValue = maxValue = arrB[0];
+
+for (i = 0; i < arrB.length; i++) {
+    if (arrB[i] < minValue) {
+        minValue = arrB[i];
+    }
+    else if (arrB[i] > maxValue) {
+        maxValue = arrB[i];
+    }
+}
+
+console.log(`Мінімальне просте число: ${minValue}`);
+console.log(`Максимальне просте число: ${maxValue}`);
+document.write(`Мінімальне просте число: ${minValue}<br>`);
+document.write(`Максимальне просте число: ${maxValue}<br>`);
